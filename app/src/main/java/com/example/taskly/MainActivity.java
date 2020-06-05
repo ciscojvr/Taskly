7a1858c0253874ef120d3e315741747386566805
Francisco Ozuna <ozuna252@hotmail.com>
2020-06-05 13:43:37 -0400

    Added methods to render main menu & respond to interactions with menu items

--- app/src/main/java/com/example/taskly/MainActivity.java
+++ app/src/main/java/com/example/taskly/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.taskly;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main Activity"; // Using TAG constant for logging
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // This method will render the menu in the main activity
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // onCreateOptionsMenu reacts to different user interactions with the menu item(s)
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                Log.d(TAG, "Add a new task");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
