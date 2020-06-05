f6a34bd48e6c188e9ad047d9814ea16f5e54d014
Francisco Ozuna <ozuna252@hotmail.com>
2020-06-05 14:26:22 -0400

    Adapted MainActivity class to store data in the database.

--- app/src/main/java/com/example/taskly/MainActivity.java
+++ app/src/main/java/com/example/taskly/MainActivity.java
@@ -4,20 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import com.example.taskly.db.TaskContract;
+import com.example.taskly.db.TaskDbHelper;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Main Activity"; // Using TAG constant for logging
+    private TaskDbHelper mHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHelper = new TaskDbHelper(this);
     }
 
     @Override
@@ -35,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "Adding a new task");
 
                 final EditText taskEditText = new EditText(this);
+
                 AlertDialog dialog = new AlertDialog.Builder(this)
                         .setTitle("Add a new task")
                         .setMessage("What do you want to do next?")
@@ -44,6 +52,15 @@ public class MainActivity extends AppCompatActivity {
                             public void onClick(DialogInterface dialog, int which) {
                                 String task = String.valueOf(taskEditText.getText());
                                 Log.d(TAG, "Task to add: " + task);
+
+                                SQLiteDatabase db = mHelper.getWritableDatabase();
+                                ContentValues values = new ContentValues();
+                                values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+                                db.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                                        null,
+                                        values,
+                                        SQLiteDatabase.CONFLICT_REPLACE);
+                                db.close();
                             }
                         })
                         .setNegativeButton("Cancel", null)
