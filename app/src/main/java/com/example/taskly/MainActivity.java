4e17931d9cf54d1d36a4bd25b14f9275315c9437
Francisco Ozuna <ozuna252@hotmail.com>
2020-06-05 13:52:01 -0400

    Created Alert Dialog to display when users press the Add menu item

--- app/src/main/java/com/example/taskly/MainActivity.java
+++ app/src/main/java/com/example/taskly/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.taskly;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Main Activity"; // Using TAG constant for logging
@@ -29,7 +32,23 @@ public class MainActivity extends AppCompatActivity {
         // onCreateOptionsMenu reacts to different user interactions with the menu item(s)
         switch (item.getItemId()) {
             case R.id.action_add_task:
-                Log.d(TAG, "Add a new task");
+                Log.d(TAG, "Adding a new task");
+
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                Log.d(TAG, "Task to add: " + task);
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
