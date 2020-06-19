d8186083c4eae72f9c3c4aa5b64e841a76e8340d
Francisco Ozuna <ozuna252@hotmail.com>
2020-06-19 15:59:54 -0400

    Created AddTaskActivity to inflate within dialog box

---
+++ app/src/main/java/com/example/taskly/AddTaskActivity.java
@@ -0,0 +1,49 @@
+package com.example.taskly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.taskly.db.TaskContract;
+import com.example.taskly.db.TaskDbHelper;
+
+import java.util.ArrayList;
+
+public class AddTaskActivity extends AppCompatActivity {
+    private static final String TAG = "Add Task Activity"; // Using TAG constant for logging
+    private TaskDbHelper mHelper;
+    private ArrayAdapter<String> mAdapter; // ArrayAdapter will help populate the ListView with the data
+    EditText enteredTask;
+    EditText enteredDueDate;
+    EditText enteredDueTime;
+    Button addTaskButton;
+    Button cancelAddTaskButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        enteredTask = (EditText) findViewById(R.id.editText_taskInfo);
+        enteredDueDate = (EditText) findViewById(R.id.editText_dateDue);
+        enteredDueTime = (EditText) findViewById(R.id.editText_timeDue);
+
+        Log.d(null,"onCreate within AddTask called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(null,"onStart within AddTask called");
+    }
+}
